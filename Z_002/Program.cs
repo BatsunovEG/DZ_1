28c8f097d1691bd305a0fa03df650e16c964ae64
Evgeniy <evgeniybatsunov@gmail.com>
2022-07-19 15:31:16 +0300

    z_002_end_2

--- Z_002/Program.cs
+++ Z_002/Program.cs
@@ -4,8 +4,10 @@
 //a = 5; b = 7 -> max = 7
 //a = 2 b = 10 -> max = 10
 //a = -9 b = -3 -> max = -3
-
-int a =1, b =2;
+Console.Write("Введите первое число  ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число  ");
+int b = Convert.ToInt32(Console.ReadLine());
 int max;
 
 if (a > b)
